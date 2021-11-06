107f64bb3edfb73864678a9174efe02930defa50
BreezyIsles <90005894+BreezyIsles@users.noreply.github.com>
2021-11-06 02:29:20 -0600

    Add files via upload

---
+++ docs/woodcuttingLocations.js
@@ -0,0 +1,31 @@
+var woodcuttingLocations = {
+	  LogX : -28.55289,
+	  LogY : -10.195313,
+	  
+	  OakX : -29.149375,
+	  OakY : -11.513672,
+	  
+	  BirchX : -28.822694, 
+	  BirchY : -15.336914,
+	  
+	  SpruceX : -32.265261,
+	  SpruceY : -12.32666,
+	  
+	  WillowX :  -19.785211,
+	  WillowY :  -29.377441,
+	  
+	  AcaciaX : -7.274206,
+	  AcaciaY : -27.333984,
+	  
+	  DreadknotX : 19.230297,
+	  DreadknotY : -48.515625,
+
+	  MahoganyX : 29.115621,
+	  MahoganyY :  -1.230469,
+	  
+	  BaobabX : 12.297691,
+	  BaobabY : -69.56543,
+	  
+	  BedlamX : 47.130728,
+	  BedlamY : 20.478516
+}
